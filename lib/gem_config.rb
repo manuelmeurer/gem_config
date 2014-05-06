9ded647a0af424dd01e219a2f5cb761c8a06d3e0
Manuel Meurer <manuel@krautcomputing.com>
2014-05-06 13:08:21 +0200

    Move require

--- lib/gem_config.rb
+++ lib/gem_config.rb
@@ -1,9 +1,8 @@
-require 'gem_config/version'
-
 module GemConfig
   InvalidKeyError = Class.new(StandardError)
 end
 
+require 'gem_config/version'
 require 'gem_config/rules'
 require 'gem_config/configuration'
 require 'gem_config/base'
